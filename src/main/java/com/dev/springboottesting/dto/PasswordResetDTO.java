a80690f3b8f3cb5c66013340418f98fb4b12db06
Jasmine Mary <jasminemary630@gmail.com>
2023-08-11 22:30:16 +0530

    reset password added

--- src/main/java/com/dev/springboottesting/dto/PasswordResetDTO.java
+++ src/main/java/com/dev/springboottesting/dto/PasswordResetDTO.java
@@ -4,15 +4,11 @@ import com.dev.springboottesting.validate.FieldMatch;
 import com.dev.springboottesting.validate.ValidateEmail;
 import com.dev.springboottesting.validate.ValidatePassword;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@FieldMatch.List({
-        @FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match")
-})
+@FieldMatch(message = "Password must match!")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class PasswordResetDTO {
     @ValidateEmail(message = "Invalid emailId")
--- src/main/java/com/dev/springboottesting/validate/FieldMatchValidator.java
+++ src/main/java/com/dev/springboottesting/validate/FieldMatchValidator.java
@@ -1,42 +1,20 @@
 package com.dev.springboottesting.validate;
 
+import com.dev.springboottesting.dto.PasswordResetDTO;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.PropertyAccessorFactory;
 
 public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
 
-    private String firstFieldName;
-    private String secondFieldName;
-
-
     @Override
-    public void initialize(final FieldMatch constraintAnnotation) {
-//        ConstraintValidator.super.initialize(constraintAnnotation);
-        firstFieldName = constraintAnnotation.first();
-        secondFieldName = constraintAnnotation.second();
+    public void initialize(FieldMatch constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
-    public boolean isValid(final Object o, final ConstraintValidatorContext constraintValidatorContext) {
-        BeanWrapper beanWrapper = PropertyAccessorFactory.forBeanPropertyAccess(o);
-        Object firstObj = beanWrapper.getPropertyValue(firstFieldName);
-        Object secondObj = beanWrapper.getPropertyValue(secondFieldName);
-
-        boolean isValid = firstObj == null && secondObj == null || firstObj != null && firstObj.equals(secondObj);
-
-        if (!isValid) {
-            constraintValidatorContext.disableDefaultConstraintViolation();
-            constraintValidatorContext.buildConstraintViolationWithTemplate(constraintValidatorContext.getDefaultConstraintMessageTemplate())
-                    .addPropertyNode(firstFieldName)
-                    .addConstraintViolation();
-        }
-
-        return isValid;
-
+    public boolean isValid(Object user, ConstraintValidatorContext constraintValidatorContext) {
+        PasswordResetDTO uPD = (PasswordResetDTO) user;
+        return uPD.getPassword().equals(uPD.getMatchingPassword());
     }
-
 
 }
